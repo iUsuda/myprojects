023c48bfda4dce86eebbb694d789c22e523da37b
iUsuda <ikuya.u1225@gmail.com>
2023-06-15 20:25:54 +0900

    first commit

---
+++ ondotori/ondotori.h
@@ -0,0 +1,356 @@
+#ifndef ONDOTORI_H_
+#define ONDOTORI_H_
+#include<vector>
+#include<fstream>
+#include<iostream>
+#include<TDatime.h>
+#include<TAxis.h>
+#include<TGraph.h>
+
+class ond
+{
+    public:
+    static int ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2);
+    static int ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, double start, double end);
+    static int ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1);
+    static int ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, double start, double end);
+    static int ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2);
+    static int ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, double start, double end);
+    static int ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1);
+    static int ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, double start, double end);
+    static int ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3);
+    static int ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3, double start, double end);
+    static void SetFormat(TGraph *g, Color_t color, Float_t alpha, std::string title = "", std::string TimeFormat = "#splitline{%m/%d}{%H:%M}", double TimeOffset = 9*3600);
+};
+
+inline int ond::ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2){
+    double time, ch1, ch2;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "\"%d-%d-%d %d:%d:%d\",\"%*lf\",\"%lf\",\"%lf\"", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2);
+
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+        v_ch2.push_back(ch2);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, double start, double end){
+    double time, ch1, ch2;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "\"%d-%d-%d %d:%d:%d\",\"%*lf\",\"%lf\",\"%lf\"", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2);
+
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+        if(time < start) continue;
+        if(time > end) break;
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+        v_ch2.push_back(ch2);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1){
+    double time, ch1;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "\"%d-%d-%d %d:%d:%d\",\"%*lf\",\"%lf\",\"%*lf\"", &y, &m, &d, &hh, &mm, &ss, &ch1);
+
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, double start, double end){
+    double time, ch1;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "\"%d-%d-%d %d:%d:%d\",\"%*lf\",\"%lf\",\"%*lf\"", &y, &m, &d, &hh, &mm, &ss, &ch1);
+
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+        if(time < start) continue;
+        if(time > end) break;
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2){
+    double time, ch1, ch2;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "%d/%d/%d %d:%d:%d,%lf,%lf", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2);
+        
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+        v_ch2.push_back(ch2);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, double start, double end){
+    double time, ch1, ch2;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "%d/%d/%d %d:%d:%d,%lf,%lf", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2);
+        
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+        if(time < start) continue;
+        if(time > end) break;
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+        v_ch2.push_back(ch2);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1){
+    double time, ch1;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "%d/%d/%d %d:%d:%d,%lf,%*lf", &y, &m, &d, &hh, &mm, &ss, &ch1);
+        
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR7wf_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, double start, double end){
+    double time, ch1;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(), "%d/%d/%d %d:%d:%d,%lf,%*lf", &y, &m, &d, &hh, &mm, &ss, &ch1);
+        
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+        if(time < start) continue;
+        if(time > end) break;
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3){
+    double time, ch1, ch2, ch3;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(),"\"%d/%d/%d %d:%d'%d\",\"%*lf\",\"%lf\",\"%lf\",\"%lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2, &ch3);
+        
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+        v_ch2.push_back(ch2);
+        v_ch3.push_back(ch3);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3, double start, double end){
+    double time, ch1, ch2, ch3;
+    int y, m, d, hh, mm, ss;
+
+    TDatime DateTime;
+
+    std::ifstream ifs(file);
+    if(ifs.fail()){
+        std::cerr << "File Open Failed : " << file << std::endl;
+        exit(1);
+    }
+    std::string buf;
+    int linecnt = 0;
+    while(getline(ifs, buf)){
+        if(linecnt < 4){
+            linecnt++;
+            continue;
+        }
+        sscanf(buf.data(),"\"%d/%d/%d %d:%d'%d\",\"%*lf\",\"%lf\",\"%lf\",\"%lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2, &ch3);
+        
+        DateTime.Set(y, m, d, hh, mm, ss);
+        time = DateTime.Convert();
+        if(time < start) continue;
+        if(time > end) break;
+
+        v_time.push_back(time);
+        v_ch1.push_back(ch1);
+        v_ch2.push_back(ch2);
+        v_ch3.push_back(ch3);
+    }
+    ifs.close();
+    return v_time.size();
+}
+
+void ond::SetFormat(TGraph *g, Color_t color, Float_t alpha, std::string title, std::string TimeFormat, double TimeOffset){
+    g->GetXaxis()->SetTimeDisplay(1);
+    g->GetXaxis()->SetTimeOffset(TimeOffset, "gmt");
+    g->GetXaxis()->SetTimeFormat(TimeFormat.c_str());
+    
+    g->SetTitle(title.c_str());
+    g->SetLineColorAlpha(color, alpha);
+    g->SetMarkerColorAlpha(color, alpha);
+    g->SetMarkerStyle(20);
+
+}
+
+#endif //ONDOTORI_H_
---
+++ ondotori/sample.cpp
@@ -0,0 +1,60 @@
+#include<TGraph.h>
+#include<TCanvas.h>
+#include"ondotori.h"
+using namespace std;
+
+void sample(){
+    string file = "./sample_TR72wf.csv";
+
+    TCanvas *c = new TCanvas("c", "c", 1600, 900);
+    gPad->SetRightMargin(0.05);
+    gPad->SetLeftMargin(0.1);
+    gPad->SetTopMargin(0.08);
+    gPad->SetBottomMargin(0.1);
+
+    //時刻指定無し
+    vector<double> v_time1;
+    vector<double> v_temp1;
+    vector<double> v_humi1;
+
+    int cnt1 = ond::ReadTR7wf(file, v_time1, v_temp1, v_humi1);
+
+    TGraph *g_temp1 = new TGraph(cnt1, v_time1.data(), v_temp1.data());
+    ond::SetFormat(g_temp1, kRed, 1, "sample1;;Temperature[#circC]");
+
+    g_temp1->GetYaxis()->SetLabelSize(0.04);
+    g_temp1->GetXaxis()->SetLabelSize(0.04);
+    g_temp1->GetXaxis()->SetLabelOffset(0.03);
+    g_temp1->GetYaxis()->SetTitleSize(0.06);
+    g_temp1->GetYaxis()->SetTitleOffset(0.6);
+
+    g_temp1->Draw("AP");
+    c->Print("sample1.pdf");
+    c->Clear();
+
+    //時刻指定あり
+    TDatime datime_start(2023, 5, 10, 0, 0, 0);
+    double time_start = datime_start.Convert();
+    TDatime datime_end(2023, 5, 15, 0, 0, 0);
+    double time_end = datime_end.Convert();
+
+    vector<double> v_time2;
+    vector<double> v_temp2;
+    vector<double> v_humi2;
+
+    int cnt2 = ond::ReadTR7wf(file, v_time2, v_temp2, v_humi2, time_start, time_end);
+
+    TGraph *g_temp2 = new TGraph(cnt2, v_time2.data(), v_temp2.data());
+    ond::SetFormat(g_temp2, kRed, 1, "sample2;;Temperature[#circC]");
+
+    g_temp2->GetYaxis()->SetLabelSize(0.04);
+    g_temp2->GetXaxis()->SetLabelSize(0.04);
+    g_temp2->GetXaxis()->SetLabelOffset(0.03);
+    g_temp2->GetYaxis()->SetTitleSize(0.06);
+    g_temp2->GetYaxis()->SetTitleOffset(0.6);
+
+    g_temp2->Draw("AP");
+    c->Print("sample2.pdf");
+    c->Clear();
+
+}
