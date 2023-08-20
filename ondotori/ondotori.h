6877d89ea18aef63e898a653eba1bb4627ea2ab6
iUsuda <ikuya.u1225@gmail.com>
2023-08-20 19:53:09 +0900

    ReadTR73U bug fix

--- ondotori/ondotori.h
+++ ondotori/ondotori.h
@@ -18,8 +18,8 @@ class ond
     static int ReadTR7_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, double start, double end);
     static int ReadTR7_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1);
     static int ReadTR7_bt(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, double start, double end);
-    static int ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3);
-    static int ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3, double start, double end);
+    static int ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch3);
+    static int ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch3, double start, double end);
     static void SetFormat(TGraph *g, Color_t color, Float_t alpha, std::string title = "", std::string TimeFormat = "#splitline{%m/%d}{%H:%M}", double TimeOffset = 9*3600);
 };
 
@@ -275,7 +275,7 @@ inline int ond::ReadTR7_bt(std::string file, std::vector<double>& v_time, std::v
     return v_time.size();
 }
 
-inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3){
+inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch3){
     double time, ch1, ch2, ch3;
     int y, m, d, hh, mm, ss;
 
@@ -293,21 +293,20 @@ inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::ve
             linecnt++;
             continue;
         }
-        sscanf(buf.data(),"\"%d/%d/%d %d:%d'%d\",\"%*lf\",\"%lf\",\"%lf\",\"%lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2, &ch3);
+        sscanf(buf.data(),"\"%d/%d/%d %d:%d'%d\",\"%*lf\",\"%lf\",\"\",\"%lf\",\"\",\"\",\"\",\"\",\"\",", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch3);
         
         DateTime.Set(y, m, d, hh, mm, ss);
         time = DateTime.Convert();
 
         v_time.push_back(time);
         v_ch1.push_back(ch1);
-        v_ch2.push_back(ch2);
         v_ch3.push_back(ch3);
     }
     ifs.close();
     return v_time.size();
 }
 
-inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch2, std::vector<double>& v_ch3, double start, double end){
+inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::vector<double>& v_ch1, std::vector<double>& v_ch3, double start, double end){
     double time, ch1, ch2, ch3;
     int y, m, d, hh, mm, ss;
 
@@ -325,7 +324,7 @@ inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::ve
             linecnt++;
             continue;
         }
-        sscanf(buf.data(),"\"%d/%d/%d %d:%d'%d\",\"%*lf\",\"%lf\",\"%lf\",\"%lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",\"%*lf\",", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch2, &ch3);
+        sscanf(buf.data(),"\"%d/%d/%d %d:%d'%d\",\"%*lf\",\"%lf\",\"\",\"%lf\",\"\",\"\",\"\",\"\",\"\",", &y, &m, &d, &hh, &mm, &ss, &ch1, &ch3);
         
         DateTime.Set(y, m, d, hh, mm, ss);
         time = DateTime.Convert();
@@ -334,7 +333,6 @@ inline int ond::ReadTR73U(std::string file, std::vector<double>& v_time, std::ve
 
         v_time.push_back(time);
         v_ch1.push_back(ch1);
-        v_ch2.push_back(ch2);
         v_ch3.push_back(ch3);
     }
     ifs.close();
